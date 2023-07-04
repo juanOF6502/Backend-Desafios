39bfc222f53b0b448146580f26746100284f894e
Juan <juan_of@live.com.ar>
2023-07-04 19:01:01 -0300

    Desafio 3

--- app.js
+++ app.js
@@ -26,18 +26,20 @@ app.get('/', (req, res) => {
 
 app.get('/products', async (req, res) => {
     const {search, limit} = req.query
-    console.log(`Buscando productos con "${search}"`)
     const products = await productManager.getProducts()
     let productstFilter = products
 
     if(search){
+        console.log(`Buscando productos con "${search}"`)
         productstFilter = products.filter(p => p.title.includes(search) || p.description.includes(search))
         res.send(productstFilter)
     }else if(limit){
+        console.log(`Buscando ${limit} producto/s`)
         const limitValue = parseInt(limit)
         productstFilter = productstFilter.slice(0, limitValue)
         res.send(productstFilter)
     } else {
+        console.log(`Buscando productos...`)
         res.send(productstFilter)
     }
 })
@@ -45,6 +47,7 @@ app.get('/products', async (req, res) => {
 app.get('/products/:pid', async (req, res) => {
     const products = await productManager.getProducts()
     const pid = parseInt(req.params.pid)
+    console.log(`Buscando producto con ID ${pid}`)
     for (const p of products){
         if(p.id === pid) {
             res.send(p)
