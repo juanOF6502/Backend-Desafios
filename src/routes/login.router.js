84aa7ae98235d36f211686b39d0dbaf002f92d29
Juan <juan_of@live.com.ar>
2023-09-18 10:26:01 -0300

    Refactor de servidor

--- src/routes/login.router.js
+++ src/routes/login.router.js
@@ -9,10 +9,15 @@ const isAuth = require('../middlewares/auth.middleware')
 const router = Router()
 
 router.get('/github', passport.authenticate(process.env.GITHUB_STRATEGY_NAME), (req, res) => {})
+
 router.get('/githubSession', passport.authenticate(process.env.GITHUB_STRATEGY_NAME), githubCallBack)
+
 router.get('/signup', (req, res) => res.render('signup'))
+
 router.get('/login', (req, res) => res.render('login'))
+
 router.get('/resetpassword', (req, res) => res.render('resetpassword'))
+
 router.get('/logout', isAuth , logout)
 
 router.post('/signup', passport.authenticate('local-signup', {
